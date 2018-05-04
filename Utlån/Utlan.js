d19d13f47b24fc38ed9b9560de0f3c67d4da7734
KingZesty <Ole.Martin11@hotmail.com>
2018-05-04 15:07:30 +0200

    Update
    La til ny Levert table, la til at du kunne levere.

--- Utlån/Utlan.js
+++ Utlån/Utlan.js
@@ -35,12 +35,13 @@ function add() {
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
     var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
     cell1.innerHTML = HilN;
     cell2.innerHTML = Utlån;
     cell3.innerHTML = dato11;
     cell4.innerHTML = dato22;
     cell5.innerHTML = utlånt;
-
+    cell6.innerHTML = '<input type="checkbox" name="tab1" />';
 
     document.getElementById("Hilnummer").value = "";
     document.getElementById("utlån").value = "";
@@ -48,15 +49,39 @@ function add() {
     document.getElementById("dato1").value = 1;
     document.getElementById("maned").value = 1;
     document.getElementById("maned1").value = 1;
+    document.getElementById("ar").value = 2018;
+    document.getElementById("ar1").value = 2018;
     document.getElementById("utlånboks").value = "";
-
-    $('#save').on('click', function () {
-    var table = $('.content')[0].outerHTML;
-    alert(table);
-});
 }
 
-
-function myDeleteFunction() {
-    document.getElementById("table").deleteRow(0);
-}
+function tab1_To_tab2()
+            {
+                var table1 = document.getElementById("table"),
+                    table2 = document.getElementById("table2"),
+                    checkboxes = document.getElementsByName("tab1");
+            console.log("Val1 = " + checkboxes.length);
+                 for(var i = 0; i < checkboxes.length; i++)
+                     if(checkboxes[i].checked)
+                        {
+                            // create new row and cells
+                            var newRow = table2.insertRow(table2.length),
+                                cell1 = newRow.insertCell(0),
+                                cell2 = newRow.insertCell(1),
+                                cell3 = newRow.insertCell(2),
+                                cell4 = newRow.insertCell(3);
+                                cell5 = newRow.insertCell(4);
+                            // add values to the cells
+                            cell1.innerHTML = table1.rows[i+1].cells[0].innerHTML;
+                            cell2.innerHTML = table1.rows[i+1].cells[1].innerHTML;
+                            cell3.innerHTML = table1.rows[i+1].cells[2].innerHTML;
+                            cell4.innerHTML = table1.rows[i+1].cells[3].innerHTML;
+                            cell5.innerHTML = table1.rows[i+1].cells[4].innerHTML;
+                            // remove the transfered rows from the first table [table1]
+                            var index = table1.rows[i+1].rowIndex;
+                            table1.deleteRow(index);
+                            // we have deleted some rows so the checkboxes.length have changed
+                            // so we have to decrement the value of i
+                            i--;
+                           console.log(checkboxes.length);
+                        }
+            }
